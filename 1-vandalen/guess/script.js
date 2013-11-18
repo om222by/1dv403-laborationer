9fc04f8cd831d8ef63c91a72b61e27eb86cf4734
OpenShift guest <om222by@student.lnu.se>
2013-11-18 08:52:09 -0500

    har fixat spelet

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,15 +2,41 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor( Math.random() * 100)+1; // Detta tal behöver bytas ut mot ett slumpat tal.
+	
+	//Math.floor( Math.random() * (max-min)+1 )+min;
+    //Math.floor( Math.random() * (100-1)+1) + 1;
+    //
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
+	
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
-		// Plats för förändring.
+		var count = 1;
+		count++;
+		
+		if (number < secret)
+		{
+			return [false, "Det hemliga bajs talet är högre!"];
+		}
 
+		else if (number > secret)
+		{
+			return [false, "Det hemliga talet är lägre!"];
+		}
+		else if (number == secret)
+		{
+			return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + count + " gissningar för att hitta det."];
+		}
+        else (number < 0 || number > 100);
+		{
+			return [false, "Talet är utanför intervallet 0 - 100"];
+		}
+		
+
+		// Plats för förändring.
 
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
