7c886c76e63b17f4ac144eb1f9177781c7c4d455
OpenShift guest <om222by@student.lnu.se>
2014-01-11 16:28:41 -0500

    fönstret är fixat med udantag för ordningen på imgheadern och laddarsymbolen

--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -1,13 +1,41 @@
 "use strict";
-var Application = {
-    
-    desktop:function() {
-        var imgViewer = document.getElementById("imageViewer");
-        imgViewer.onClick();
+function popupWindow () {
+        var surface = document.getElementById("desktop");
+        var labelPic = document.createElement("img");
+        var label = document.createElement("p");
+        var close = document.createElement("p");
+        var windowHeader = document.createElement("div");
+        var status = document.createElement("p");
+        var layout = document.createElement("div");
+        var popup = document.createElement("div"); //den nya instansen
+        
+        labelPic.className = "labelPic";
+        label.className = "label";
+        close.className = "close";
+        windowHeader.className="windowHeader";
+        status.className ="status";
+        layout.className ="layout";
+        popup.className = "popup";
+        
+        labelPic.src = ("logga.png");
+        
+        label.innerHTML = "Image Viewer";
+        close.innerHTML = "x";
+        
+        windowHeader.appendChild(labelPic);
+        windowHeader.appendChild(label);
+        windowHeader.appendChild(close);
+        popup.appendChild(windowHeader);
+        popup.appendChild(layout);
+        popup.appendChild(status);
+        surface.appendChild(popup);
     }
-    
-};
 
 window.onload = function () {
-    Application.desktop();  
+    var imgViewer = document.getElementById("imageViewer");
+        
+        imgViewer.onclick = function(e){
+            popupWindow();
+            return false;
+        };
 };
