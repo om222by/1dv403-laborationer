53726afc22ed90873a8cc815be256f8a87ed13d3
OpenShift guest <om222by@student.lnu.se>
2014-02-02 17:06:59 -0500

    memory upplagt

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,35 +2,145 @@
 
 var Memory = {
     
-    pictures: [],
+    game_board: document.createElement("div"),
     
-    init:function (e)
+    //spelplanen, där de vända kortens "bilder" hamnar
+    memoryBoard: [],
+    
+    //gissade korten
+    clickedCards: [],
+    
+    correctGuesses: 0,
+    counter: 0,
+    
+    ///varibler att spara element i
+    cardDiv: null,
+    cardLink: null,
+    cardPicture: null,
+    
+    init: function ()
     {
+        Memory.memoryBoard = new RandomGenerator.getPictureArray(4,4);
         
-        Memory.pictures = new RandomGenerator.getPictureArray(3,4);
-        console.log(Memory.pictures);
+        var sendToTryCard = function(j) {return function() {Memory.tryCard(j)}};
         
-        var img = document.createElement("img");
-        var plan = document.getElementById("spelplan");
-        var table = document.createElement("table");
-        var tr = document.createElement("tr");
-        var td = document.createElement("td");
+        Memory.game_board.setAttribute("id", "game_board");
         
-        plan.appendChild(table);
-        table.appendChild(tr);
-        
-        for (var i = 0; i < 3; i++) {
-            tr.appendChild(td);
+        for (var i = 0; i < Memory.memoryBoard.length; i +=1){
             
-                for (var y = 0; y < 4; y++) {
-                    td.appendChild(img);
-                }
+            
+            Memory.cardDiv = document.createElement("div");
+            Memory.cardDiv.setAttribute("class", "card");
+            
+            Memory.cardPicture = document.createElement("img");
+            Memory.cardPicture.setAttribute("src", "pics/0.png");
+            
+            Memory.cardLink = document.createElement("a");
+            Memory.cardLink.setAttribute("class", i);
+            Memory.cardLink.addEventListener("click", sendToTryCard(i), false);
+            
+            
+            Memory.cardLink.appendChild(Memory.cardPicture);
+            Memory.cardDiv.appendChild(Memory.cardLink);
+            
+            Memory.game_board.appendChild(Memory.cardDiv);
+            
+            
+            
         }
         
-    }
+        document.appendChild(Memory.game_board);
+        
+        
+        //for (var i = 0; i < 3; i++) {
+        //    tr.appendChild(td);
+        //    for (var y = 0; y < 4; y++) {
+        //        
+        //        for (var picture in Memory.Pictures){
+        //               img.setAttribute("src", "pics/0.png");
+        //                a.appendChild(img);
+        //        }
+        //        td.appendChild(a);
+        //    }
+        //}
+    },
     
+    tryCard: function (thisCardNumber) {
+        
+        Memory.clickedCards.push(thisCardNumber);
+        
+        //vänd valt kort
+        var thisCardPicture = document.getElementById(thisCardNumber);
+        thisCardPicture.setAttribute("src", "pics/" + Memory.memoryBoard[thisCardNumber] + ".png");
+        
+        //när två kort är vända
+        if(Memory.clickedCards.length == 2 && Memory.clickedCards[0] != Memory.clickedCards[1]){
+            
+            //kollar om gissningen är korrekt
+            Memory.checkIfCorrect(Memory.memoryBoard[Memory.clickedCards[0]], Memory.memoryBoard[Memory.clickedCards[1]]);
+            
+            //ökar counter och avmarkerar korten
+            Memory.counter+=1;
+            document.getElementById("counter").innerHTML = "Antal gissningar: " + Memory.counter;
+            Memory.clickedCards.length = 0;
+        }
+            
+        //Om användaren klickar på samma kort två gånger
+        else if(Memory.clickedCards[0] == Memory.clickedCards[1])
+        {
+            Memory.clickedCards.pop();
+        }
+            
+        //är det inga nedvända kort kvar?
+        if(Memory.correctGuesses == Memory.memoryBoard.length)
+        {
+            document.body.removeChild(document.getElementById("game_board"));
+            document.body.removeChild(document.getElementById("counter"));
+            document.getElementById("you_won").innerHTML = "Grattis! Du vann efter "+ Memory.guesses +" gissningar!";
+            
+        }
+        
+    },
+    
+    checkIfCorrect: function (card1, card2) {
+        
+        var picture1;
+        var picture2;
+        
+        if(card1 == card2) {
+            
+            Memory.correctGuesses +=2;
+            
+            //Låter ParentNoden döda länkarna i barnen
+            var link1 = document.getElementsByClassName(Memory.clickedCards[0]);
+            link1.parentNode.removeChild(link1[0]);
+            var link2 = document.getElementsByClassName(Memory.clickedCards[0]);
+            link2.parentNode.removeChild(link2[0]);
+            
+            
+        }
+        else {
+            picture1 = document.getElementById(Memory.clickedCards[0]);
+            picture2 = document.getElementById(Memory.clickedCards[1]);
+            
+            //vänder tillbaka korten efter 1 sec
+            setTimeout(function() {picture1.src = "pics/0.png";}, 1000);
+            setTimeout(function() {picture2.src = "pics/0.png";}, 1000);
+        }
+    }
 };
 
-window.onload = function() {
-  Memory.init();  
-};
+window.onload = Memory.init();  
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -26,115 +26,113 @@ var PWD = {
         a.onclick = function(e){
         
         counter +=1;
-                if(counter > 1)
-                {
-                    return false;
+        
+        if(counter > 1)
+        {
+            return false;
+        }
+        
+        var labelPic = document.createElement("img");
+        var labelPicP = document.createElement("p");
+        var label = document.createElement("p");
+        var close = document.createElement("p");
+        var closeImg = document.createElement("img");
+        var closeLink = document.createElement("a");
+        var windowHeader = document.createElement("div");
+        var status = document.createElement("p");
+        var ajaxLoader = document.createElement("img");
+        var layout = document.createElement("div");
+        var popup = document.createElement("div"); //den nya instansen
+        var url = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";    
+        
+        labelPic.className = "labelPic";
+        label.className = "label";
+        close.className = "close";
+        windowHeader.className="windowHeader";
+        status.className ="status";
+        layout.className ="layout";
+        popup.className = "popup";
+        
+        closeLink.setAttribute("href","#");
+        labelPic.src = ("logga.png");
+        ajaxLoader.src =("ajax-loader.gif");
+        closeImg.src =("close.png");
+        
+        label.innerHTML = "Image Viewer";
+        
+        //lägger in de olika elementen i varandra
+        closeLink.appendChild(closeImg);
+        close.appendChild(closeLink);
+        labelPicP.appendChild(labelPic);
+        windowHeader.appendChild(labelPicP);
+        windowHeader.appendChild(label);
+        windowHeader.appendChild(close);
+        status.appendChild(ajaxLoader);
+        popup.appendChild(windowHeader);
+        popup.appendChild(layout);
+        popup.appendChild(status);
+        surface.appendChild(popup);
+        
+        closeLink.onclick = function () {
+            surface.removeChild(popup);
+            counter = 0;
+        };
+        
+        new AjaxCon(url, function (data) {
+            
+            var jsonImages = JSON.parse(data);
+            var width = 0;
+            var height = 0;
+            var i = null;
+            var imgCounter = document.getElementsByTagName("div");
+            
+            //skapar divar baserat på arrayens längd
+            for (i=0; i < jsonImages.length; i+=1){
+                
+                var ajaxImageDiv = document.createElement("div");
+                
+                ajaxImageDiv.className = "ajaxImg";
+                layout.appendChild(ajaxImageDiv);
+                
+                //sätter bredden på tumnaglarna
+                if(jsonImages[i].thumbWidth > width){
+                    
+                    width = jsonImages[i].thumbWidth;
                 }
                 
-                var labelPic = document.createElement("img");
-                var labelPicP = document.createElement("p");
-                var label = document.createElement("p");
-                var close = document.createElement("p");
-                var closeImg = document.createElement("img");
-                var closeLink = document.createElement("a");
-                var windowHeader = document.createElement("div");
-                var status = document.createElement("p");
-                var ajaxLoader = document.createElement("img");
-                var layout = document.createElement("div");
-                var popup = document.createElement("div"); //den nya instansen
-                var url = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";    
-                
-                labelPic.className = "labelPic";
-                label.className = "label";
-                close.className = "close";
-                windowHeader.className="windowHeader";
-                status.className ="status";
-                layout.className ="layout";
-                popup.className = "popup";
-                
-                closeLink.setAttribute("href","#");
-                labelPic.src = ("logga.png");
-                ajaxLoader.src =("ajax-loader.gif");
-                closeImg.src =("close.png");
-                
-                label.innerHTML = "Image Viewer";
-                
-                
-                closeLink.appendChild(closeImg);
-                close.appendChild(closeLink);
-                labelPicP.appendChild(labelPic);
-                windowHeader.appendChild(labelPicP);
-                windowHeader.appendChild(label);
-                windowHeader.appendChild(close);
-                status.appendChild(ajaxLoader);
-                popup.appendChild(windowHeader);
-                popup.appendChild(layout);
-                popup.appendChild(status);
-                surface.appendChild(popup);
-                
-                
-                
-                closeLink.onclick = function () {
-                    surface.removeChild(popup);
-                    counter = 0;
-                };
-                
-                new AjaxCon(url, function (data) {
+                //sätter höjden på tumnaglarna
+                if(jsonImages[i].thumbHeight > height){
                     
-                    var jsonImages = JSON.parse(data);
-                    var width = 0;
-                    var height = 0;
-                    var i = null;
-                    var imgCounter = document.getElementsByTagName("div");
-                    
-                    for (i=0; i < jsonImages.length; i+=1){
+                    height = jsonImages[i].thumbHeight;
+                }
+                
+                var ajaxImages = document.createElement("img");
+                
+                ajaxImages.setAttribute("src",jsonImages[i].thumbURL);
+                
+                var ajaxImageLinks = document.createElement("a");
+                ajaxImageLinks.setAttribute("href","#");
+                
+                ajaxImageLinks.appendChild(ajaxImages);
+                ajaxImageDiv.appendChild(ajaxImageLinks);
+                
+                PWD.setBackground(jsonImages[i].URL, ajaxImageLinks);
+                
+            }
+                
+                for (var x = 0; x < imgCounter.length; x+=1){
+                    if (imgCounter[x].className === "ajaxImg") {
                         
-                        var ajaxImageDiv = document.createElement("div");
-                        
-                        ajaxImageDiv.className = "ajaxImg";
-                        layout.appendChild(ajaxImageDiv);
-                        
-                        //sätter bredden på tumnaglarna
-                        if(jsonImages[i].thumbWidth > width){
-                            
-                            width = jsonImages[i].thumbWidth;
-                        }
-                        
-                        //sätter höjden på tumnaglarna
-                        if(jsonImages[i].thumbHeight > height){
-                            
-                            height = jsonImages[i].thumbHeight;
-                        }
-                        
-                        var ajaxImages = document.createElement("img");
-                        
-                        ajaxImages.setAttribute("src",jsonImages[i].thumbURL);
-                        
-                        var ajaxImageLinks = document.createElement("a");
-                        
-                        ajaxImageLinks.setAttribute("href","#");
-                        
-                        ajaxImageLinks.appendChild(ajaxImages);
-                        
-                        ajaxImageDiv.appendChild(ajaxImageLinks);
-                        
-                        PWD.setBackground(jsonImages[i].URL, ajaxImageLinks);
-                        
+                        imgCounter[x].style.width = width + "px";
+                        imgCounter[x].style.height = height + "px";
                     }
-                        
-                        for (var x = 0; x < imgCounter.length; x+=1){
-                            if (imgCounter[x].className === "ajaxImg") {
-                                
-                                imgCounter[x].style.width = width + "px";
-                                imgCounter[x].style.height = height + "px";
-                            }
-                        }
-                        
-                        status.removeChild(ajaxLoader);
-                        
-                });
-                return false;
-            
+                }
+                
+                status.removeChild(ajaxLoader);
+                
+        });
+        return false;
+        
         };
     },
     
